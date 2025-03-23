e449bc29101189953af515a3a7980153266e2256
Hui Kang Tong <tonghuikang@outlook.com>
2025-03-22 18:43:18 -0700

    tutorial complete I guess

--- train_grpo_tutorial.py
+++ train_grpo_tutorial.py
@@ -46,6 +46,7 @@ def train_grpo():
     wandb.init(project="grpo-training", name="Qwen2-0.5B")
 
     dataset = load_dataset("trl-lib/tldr", split="train")
+    dataset = dataset.select(range(1600))
 
     # Define the reward function, which rewards completions that are close to 20 characters
     def reward_func(completions, **kwargs):
@@ -63,15 +64,25 @@ def train_grpo():
         return [count(completion) for completion in completions]
 
     training_args = GRPOConfig(
-        # output_dir="DeepSeek-R1-Distill-Qwen-1.5B-GRPO",
-        output_dir="Qwen2-0.5B-GRPO",
-        logging_steps=10,
-        gradient_accumulation_steps=4,
+        # length limits (which needs to be changed for these to be useful)
+        max_completion_length=1024,
+        max_prompt_length=1024,
+        # training config, see docstring in GRPOTrainer._get_train_sampler
+        # I want a huge number of generations so I can calculate the reward for all of them
+        # Currently, I am forced to have lots of GPUs before I can have a huge number of generations
         num_generations=16,
-        per_device_train_batch_size=16,
-        report_to="wandb",
+        per_device_train_batch_size=16,  # num_devices * this_number should be num_generations
         num_train_epochs=1.0,
+        gradient_accumulation_steps=16,
+        # output_dir="DeepSeek-R1-Distill-Qwen-1.5B-GRPO",
+        num_iterations=1,  # this means reusing completions?
+        output_dir="Qwen2-0.5B-GRPO",
+        logging_steps=16,
+        # vllm configs
         use_vllm=True,
+        # logging configs
+        report_to="wandb",
+        log_completions=True,
     )
     trainer = GRPOTrainer(
         # model="deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B",
