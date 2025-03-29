2ede08a7b8d856766b8d5d4ac7fab4a7b6a1ce44
Hui Kang Tong <tonghuikang@outlook.com>
2025-03-29 01:00:44 -0700

    form training

--- train_grpo.py
+++ train_grpo.py
@@ -4,7 +4,7 @@ from train_config import (
     REFERENCE_MODEL_NAME,
     TRAIN_CUDA_VISIBLE_DEVICES,
     NUM_GENERATIONS,
-    TRAIN_NUM_GPUS,
+    CAPACITY_PER_GPU,
     MAX_PROMPT_LENGTH,
     MAX_COMPLETION_LENGTH,
 )
@@ -53,17 +53,42 @@ if __name__ == "__main__":
 
     import math
 
-    def length_preference_function(length, preferred_length=256, ceiling=1.0, scaling=1/3):
+    def length_preference_function(
+        length, preferred_length=256, ceiling=1.0, scaling=1
+    ):
         # maps [0,inf] -> [0,1]
         x = length / preferred_length
         y = math.e * x * math.exp(-x)
         return (min(y, ceiling)) ** (scaling) / ceiling
 
-    def formatting_func(prompts, completions, correct_answer, **kwargs):
+    def length_thinking_func(prompts, completions, correct_answer, **kwargs):
+        # the thinking part should be around 256 tokens
+
+        def func(completion):
+            if "</think>" not in completion:
+                return 0
+            relevant_index = completion.index("</think>")
+            completion_length = len(tokenizer.encode(completion[:relevant_index]))
+            return length_preference_function(completion_length, scaling=1)
+
+        return [func(completion) for completion in completions]
+
+    def length_total_func(prompts, completions, correct_answer, **kwargs):
+        # the total output should be around 256 tokens
+
+        def func(completion):
+            completion_length = len(tokenizer.encode(completion))
+            return length_preference_function(completion_length, scaling=1)
+
+        return [func(completion) for completion in completions]
+
+    def format_thinking_func(prompts, completions, correct_answer, **kwargs):
         # each line of thinking text should be 100 characters
 
         def func(completion):
             if "</think>" not in completion:
+                return 0
+            if "oxed{" not in completion and "```python" not in completion:
                 return 0
             completion = completion[: completion.index("</think>")].strip()
 
@@ -72,63 +97,67 @@ if __name__ == "__main__":
             numerator = 1
             for line in lines:
                 length = len(line)
-                score = length_preference_function(length, preferred_length=100, scaling=1)
+                score = length_preference_function(
+                    length, preferred_length=100, scaling=1
+                )
                 numerator += score * length
                 denominator += length
             return numerator / denominator
 
         return [func(completion) for completion in completions]
 
-    import math
-
-    def length_func(prompts, completions, correct_answer, **kwargs):
-        # the output should be around 256 tokens
-
-        def func(completion):
-            relevant_index = len(completion)
-            contains_python_opening = "```python" in completion
-            if contains_python_opening:
-                relevant_index = completion.index("```python")
-            completion_length = len(tokenizer.encode(completion[:relevant_index]))
-            return length_preference_function(completion_length)
-
-        return [func(completion) for completion in completions]
 
     import re
 
+    def correctness_function(completion, correct_answer):
+        match = re.search(r"\\boxed\{(.*?)\}", completion)
+        boxed_content = match.group(1) if match else ""
+
+        answer_attempt = bool(boxed_content != "")
+        answer_correct = boxed_content == str(correct_answer)
+        contains_python_opening = "```python" in completion
+        if not answer_attempt and not contains_python_opening:
+            return -0.5
+
+        if not answer_attempt:
+            # penalize long sequences, likely attempting to solve on its own
+            # not sure if it affects the math sequences
+            return 0
+
+        if not answer_correct:
+            return -1
+
+        # attempted answer and is correct
+        return 1
+
     correctness_func_counter = [0]
+    TOTAL_CALLS_ESTIMATED = (
+        len(dataset) // 2 * NUM_GENERATIONS
+    )  # evaluation counts not considered
+    CALLS_UNTIL_CONSIDER_CORRECTNESS = TOTAL_CALLS_ESTIMATED // 2
+    CALLS_UNTIL_CONSIDER_CORRECTNESS_FULLY = CALLS_UNTIL_CONSIDER_CORRECTNESS // 2
 
     def correctness_func(prompts, completions, correct_answer, **kwargs):
         # the answer should be correct
 
-        # do not consider correctness until a certain number of calls
         correctness_func_counter[0] += 1
-        if correctness_func_counter[0] < 10_000:
-            return [0 for _ in completions]  # Return a list of zeros with the same length as completions
-
-        def func(completion, correct_answer):
-            match = re.search(r"\\boxed\{(.*?)\}", completion)
-            boxed_content = match.group(1) if match else ""
-
-            answer_attempt = bool(boxed_content != "")
-            answer_correct = boxed_content == str(correct_answer)
-            contains_python_opening = "```python" in completion
-            if not answer_attempt and not contains_python_opening:
-                return -0.5
-
-            if not answer_attempt:
-                # penalize long sequences, likely attempting to solve on its own
-                # not sure if it affects the math sequences
-                return 0
-
-            if not answer_correct:
-                return -1
-
-            # attempted answer and is correct
-            return 1
+        factor = (correctness_func_counter[0] - CALLS_UNTIL_CONSIDER_CORRECTNESS) / (
+            CALLS_UNTIL_CONSIDER_CORRECTNESS_FULLY - CALLS_UNTIL_CONSIDER_CORRECTNESS
+        )
+        if correctness_func_counter[0] <= CALLS_UNTIL_CONSIDER_CORRECTNESS:
+            # do not consider correctness until a certain number of calls
+            factor = 0
 
         return [
-            func(completion, correct_answer_)
+            factor * correctness_function(completion, correct_answer_)
+            for completion, correct_answer_ in zip(completions, correct_answer)
+        ]
+
+    def correctness_ref(prompts, completions, correct_answer, **kwargs):
+        # use to log correctness without affecting reward value
+        factor = 0.001
+        return [
+            factor * correctness_function(completion, correct_answer_)
             for completion, correct_answer_ in zip(completions, correct_answer)
         ]
 
@@ -149,7 +178,7 @@ if __name__ == "__main__":
         # Breaking them down into separate generations is not the same,
         # because that results in a different advantage calculation
         num_generations=NUM_GENERATIONS,
-        per_device_train_batch_size=NUM_GENERATIONS // TRAIN_NUM_GPUS,
+        per_device_train_batch_size=CAPACITY_PER_GPU,
         # num_devices * this_number should be num_generations
         gradient_accumulation_steps=num_iterations,
         num_train_epochs=1.0,
@@ -158,7 +187,7 @@ if __name__ == "__main__":
         output_dir=f"DeepSeek-R1-Distill-Qwen-1.5B-GRPO-{num_iterations}-{gradient_accumulation_steps}-{learning_rate:.0e}",
         logging_steps=1,
         lr_scheduler_type="cosine",
-        warmup_steps=7,
+        warmup_steps=32,
         learning_rate=learning_rate,
         epsilon_high=0.28,
         beta=0.1,
@@ -177,9 +206,11 @@ if __name__ == "__main__":
     trainer = GRPOTrainer(
         model=model,
         reward_funcs=[
-            formatting_func,
-            length_func,
+            format_thinking_func,
+            length_thinking_func,
+            length_total_func,
             correctness_func,
+            correctness_ref,
         ],
         args=training_args,
         train_dataset=dataset,
